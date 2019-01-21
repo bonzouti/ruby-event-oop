15fb53419e1ba51961e4932c5e9b8d09a4e5c8c5
pierremathieu18@gmail.com <mathieupierre@MacBook-Air-de-MATHIEU.local>
2019-01-21 19:09:45 +0100

    calendar

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,44 @@
+require "pry"
+require "time"
 class Event
-
+  attr_accessor :start_date, :length, :title, :attendees
+  emails_array = []
+  def initialize(start_date, length, title, attendees)
+    @start = Time.parse(start_date)
+    @length = length.to_i
+    @title = title.to_s
+    @emails = attendees
+  end
+    
+  def postpone_24
+    postpone = 60*60*24
+    @start += postpone
+    return @start
+  end
+    
+def end_date
+  @end_date = @start + @length
 end
+
+def is_past
+  @start < Time.now ? true : false
+end
+
+def is_future
+  !itself.is_past
+end
+
+def is_soon
+  Time.now - @start < 30*60 ? true : false
+end
+def to_s
+  puts "Titre : #{@title}"
+  puts "Date de début :  #{@start}"
+  puts "Durée : #{@length} minutes" 
+  puts "Invités : #{@emails}"
+  
+end
+end
+
+binding.pry 
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class User
+  attr_accessor :email
+  @@array_list = Array.new
+ 
+
+  def initialize(email_to_save)
+    @email = email_to_save
+    @array_list << self
+  end
+
+  def update_email(email_to_update)
+    @email = email_to_update
+  end
+
+
+  def self_all
+    return self_all
+    
+  end
+
+  def show_itself
+    puts self
+    
+  end
+
+  def self.count
+    return @@user_count
+  end
 
 end
+
+#binding.pry
